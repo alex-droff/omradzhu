d31ee6b47345fc0e273d4ff69b178827ce27f7e5
Alex Droff <alex.droff@gmail.com>
2022-06-06 15:15:55 +0300

    ссылки на формы

--- wp-content/themes/omradzhu/js/scripts.js
+++ wp-content/themes/omradzhu/js/scripts.js
@@ -74,6 +74,20 @@
             }, 1000);
         })
 
+        $('a:not(.spu-clickable)[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') ||
+                location.hostname == this.hostname) {
+                var target = jQuery(this.hash);
+                target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    jQuery("html, body").animate({
+                        scrollTop: target.offset().top - 90
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+
 
     });
 })(jQuery);
